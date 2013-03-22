94d9a03d620edd31bbc27f12a9e952dee53df532
silconsystem <lysergic_evangelion@live.nl>
2013-03-22 18:28:14 +0100

    did some work on the flowchart

--- Gensokyo_Libgdx.mm
+++ Gensokyo_Libgdx.mm
@@ -1,52 +1,626 @@
 <map version="0.9.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Gensokyo_Libgdx" ID="ID_487223567" CREATED="1363175700761" MODIFIED="1363175724280">
+<node ID="ID_487223567" CREATED="1363175700761" MODIFIED="1363550287642">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5"><i><b>Gensokyo_Libgdx </b></i></font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      2d Danmaku shooter with unlockable content.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Game Story:
+    </p>
+    <p>
+      -Still Brewing
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Game concept:
+    </p>
+    <p>
+      -Start with one or two playable characters and kill enemies to gain credits with which you can unlock more characters.
+    </p>
+    <p>
+      -Unlock weapon types and combine weapons from other players.
+    </p>
+    <p>
+      -Unlock shield types that also hold different bomb spells.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Game controls:
+    </p>
+    <p>
+      -for Android touch and accelerometer controls.
+    </p>
+    <p>
+      -for Desktop and HTML5 keyboard controls and if possible joystick(user configurable).
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#160;&#160;-NOTE: Because touch controls are not good for this kind of games the main focus should
+    </p>
+    <p>
+      &#160;&#160;&#160;be on using the accelerometer to control the player and the player should auto-fire if touch
+    </p>
+    <p>
+      &#160;&#160;&#160;or accelerometer controls are chosen.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      for now theres three espisodes, you can select the one you want to start with.
+    </p>
+    <p>
+      after these three are completed theres a final stage (like Fairy Wars).
+    </p>
+  </body>
+</html>
+</richcontent>
 <hook NAME="MapStyle" max_node_width="600"/>
-<node TEXT="packages" POSITION="right" ID="ID_715184394" CREATED="1363175727036" MODIFIED="1363175732674">
-<node TEXT="com.silconsystem.gensokyo" ID="ID_1888006722" CREATED="1363175739931" MODIFIED="1363175761959">
-<node TEXT="GensokyoActivity.java" ID="ID_960719564" CREATED="1363176037386" MODIFIED="1363176059646"/>
-<node TEXT="GensokyoGame.java" ID="ID_1940267429" CREATED="1363176061655" MODIFIED="1363176072246"/>
+<node POSITION="right" ID="ID_715184394" CREATED="1363175727036" MODIFIED="1363549669161">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5" color="#cc3300"><b>packages</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_1888006722" CREATED="1363175739931" MODIFIED="1363549697529">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4" color="#660066"><b>com.silconsystem.gensokyo</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_960719564" CREATED="1363176037386" MODIFIED="1363549820999">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>GensokyoActivity.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node TEXT="com.silconsystem.gensokyo.actors" ID="ID_1447339363" CREATED="1363175767249" MODIFIED="1363175785296">
-<node TEXT="Level.java" ID="ID_2219808" CREATED="1363176074262" MODIFIED="1363176126607"/>
-<node TEXT="PlayerCharacter.java" ID="ID_466638874" CREATED="1363176132696" MODIFIED="1363176144876"/>
-<node TEXT="PlayerItem.java" ID="ID_788695347" CREATED="1363176146526" MODIFIED="1363176157102"/>
-<node TEXT="PlayerModel.java" ID="ID_1170264378" CREATED="1363176158573" MODIFIED="1363176165885"/>
-<node TEXT="Profile.java" ID="ID_795694344" CREATED="1363176168452" MODIFIED="1363176179336"/>
-<node TEXT="Reward.java" ID="ID_1812547998" CREATED="1363176180851" MODIFIED="1363176188521"/>
-<node TEXT="Shield.java" ID="ID_384247147" CREATED="1363176191649" MODIFIED="1363176197535"/>
-<node TEXT="Shot.java" ID="ID_1101248419" CREATED="1363176200529" MODIFIED="1363176204718"/>
+<node ID="ID_1940267429" CREATED="1363176061655" MODIFIED="1363549829407">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>GensokyoGame.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node TEXT="com.silconsystem.gensokyo.actors.scene2d" ID="ID_70186131" CREATED="1363175792165" MODIFIED="1363175821657">
-<node TEXT="Player2d.java" ID="ID_1783539345" CREATED="1363176214527" MODIFIED="1363176224826"/>
-<node TEXT="Enemy2d.java" ID="ID_1430330156" CREATED="1363176227093" MODIFIED="1363176235929"/>
 </node>
-<node TEXT="com.silconsystem.gensokyo.managers" ID="ID_1855670013" CREATED="1363175847839" MODIFIED="1363175899111">
-<node TEXT="AudioFxManager.java" ID="ID_571933847" CREATED="1363176238268" MODIFIED="1363176268164"/>
-<node TEXT="AudioManager.java" ID="ID_1947233616" CREATED="1363176253083" MODIFIED="1363176262928"/>
-<node TEXT="LevelManager.java" ID="ID_1897641073" CREATED="1363176269529" MODIFIED="1363176279588"/>
-<node TEXT="PreferencesManager.java" ID="ID_1529164285" CREATED="1363176280576" MODIFIED="1363176291085"/>
-<node TEXT="ProfileManager.java" ID="ID_1125447305" CREATED="1363176294212" MODIFIED="1363176303204"/>
+<node ID="ID_1447339363" CREATED="1363175767249" MODIFIED="1363549721444">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>com.silconsystem.gensokyo.actors</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_2219808" CREATED="1363176074262" MODIFIED="1363549841314">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Level.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node TEXT="com.silconsystem.gensokyo.stages" ID="ID_1066623023" CREATED="1363175865655" MODIFIED="1363176008868">
-<node TEXT="EspisodeOne.java" ID="ID_1239331252" CREATED="1363176314012" MODIFIED="1363176326897"/>
+<node ID="ID_466638874" CREATED="1363176132696" MODIFIED="1363549850822">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>PlayerCharacter.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node TEXT="com.silconsystem.gensokyo.utils" ID="ID_218065092" CREATED="1363175900657" MODIFIED="1363175913550">
-<node TEXT="DefaultActorListener.java" ID="ID_1605682546" CREATED="1363176329858" MODIFIED="1363176369420"/>
-<node TEXT="LRUCache.java" ID="ID_351160100" CREATED="1363176370497" MODIFIED="1363176378461"/>
-<node TEXT="Range.java" ID="ID_144470217" CREATED="1363176380261" MODIFIED="1363176385691"/>
-<node TEXT="RangeF.java" ID="ID_1570036423" CREATED="1363176388636" MODIFIED="1363176393498"/>
-<node TEXT="TextUtils.java" ID="ID_1119776213" CREATED="1363176396003" MODIFIED="1363176402609"/>
-<node TEXT="VectorUtils.java" ID="ID_233637856" CREATED="1363176405178" MODIFIED="1363176411736"/>
+<node ID="ID_788695347" CREATED="1363176146526" MODIFIED="1363549859941">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>PlayerItem.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node TEXT="com.silconsystem.gensokyo.screens" ID="ID_144154300" CREATED="1363175916394" MODIFIED="1363176031985">
-<node TEXT="AbstractScreen.java" ID="ID_822242640" CREATED="1363176420385" MODIFIED="1363176432046"/>
-<node TEXT="HighScores.java" ID="ID_502063537" CREATED="1363176434031" MODIFIED="1363176442045"/>
-<node TEXT="LevelScreen.java" ID="ID_1398478690" CREATED="1363176444702" MODIFIED="1363176452444"/>
-<node TEXT="MenuScreen.java" ID="ID_660781903" CREATED="1363176454496" MODIFIED="1363176460611"/>
-<node TEXT="SettingScreen.java" ID="ID_557571359" CREATED="1363176462654" MODIFIED="1363176472804"/>
-<node TEXT="SplashScreen.java" ID="ID_811890694" CREATED="1363176474588" MODIFIED="1363176482352"/>
-<node TEXT="StartGameScreen.java" ID="ID_143515849" CREATED="1363176484362" MODIFIED="1363176493055"/>
+<node ID="ID_1170264378" CREATED="1363176158573" MODIFIED="1363549872666">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>PlayerModel.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_795694344" CREATED="1363176168452" MODIFIED="1363549906399">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Profile.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1812547998" CREATED="1363176180851" MODIFIED="1363549915904">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Reward.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_384247147" CREATED="1363176191649" MODIFIED="1363549924918">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Shield.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1101248419" CREATED="1363176200529" MODIFIED="1363549934774">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Shot.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node ID="ID_70186131" CREATED="1363175792165" MODIFIED="1363549750259">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="3" color="#660066"><b>com.silconsystem.gensokyo.actors.scene2d</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_1783539345" CREATED="1363176214527" MODIFIED="1363549944519">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Player2d.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1430330156" CREATED="1363176227093" MODIFIED="1363549951975">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Enemy2d.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node ID="ID_1855670013" CREATED="1363175847839" MODIFIED="1363549761081">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>com.silconsystem.gensokyo.managers</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_571933847" CREATED="1363176238268" MODIFIED="1363549965346">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>AudioFxManager.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1947233616" CREATED="1363176253083" MODIFIED="1363549975431">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>AudioManager.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1897641073" CREATED="1363176269529" MODIFIED="1363549992613">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>LevelManager.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1529164285" CREATED="1363176280576" MODIFIED="1363550011052">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>PreferencesManager.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1125447305" CREATED="1363176294212" MODIFIED="1363550020050">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>ProfileManager.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node ID="ID_1066623023" CREATED="1363175865655" MODIFIED="1363549779302">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>com.silconsystem.gensokyo.stages</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_1239331252" CREATED="1363176314012" MODIFIED="1363550032297">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>EspisodeOne.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node ID="ID_218065092" CREATED="1363175900657" MODIFIED="1363549791571">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>com.silconsystem.gensokyo.utils</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_1605682546" CREATED="1363176329858" MODIFIED="1363550040640">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>DefaultActorListener.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_351160100" CREATED="1363176370497" MODIFIED="1363550048216">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>LRUCache.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_144470217" CREATED="1363176380261" MODIFIED="1363550056782">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>Range.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1570036423" CREATED="1363176388636" MODIFIED="1363550065845">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>RangeF.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1119776213" CREATED="1363176396003" MODIFIED="1363550073725">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>TextUtils.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_233637856" CREATED="1363176405178" MODIFIED="1363550082157">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>VectorUtils.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node ID="ID_144154300" CREATED="1363175916394" MODIFIED="1363549801946">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>com.silconsystem.gensokyo.screens</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_822242640" CREATED="1363176420385" MODIFIED="1363550090884">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>AbstractScreen.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_502063537" CREATED="1363176434031" MODIFIED="1363550099241">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>HighScores.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1398478690" CREATED="1363176444702" MODIFIED="1363550109184">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>LevelScreen.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_660781903" CREATED="1363176454496" MODIFIED="1363550117488">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>MenuScreen.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_557571359" CREATED="1363176462654" MODIFIED="1363550125663">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>SettingScreen.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_811890694" CREATED="1363176474588" MODIFIED="1363550133202">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>SplashScreen.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_143515849" CREATED="1363176484362" MODIFIED="1363550141629">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff"><b>StartGameScreen.java</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 </node>
